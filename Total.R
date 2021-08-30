49e9859e096efa73c1be61056ce7511caa6fc2d1
dilanvergara <58192981+dilanvergara@users.noreply.github.com>
2021-08-30 08:37:12 -0500

    Update Total.R

--- Total.R
+++ Total.R
@@ -4,14 +4,12 @@ Datos
 library(stringr)
 library(readr)
 library(dplyr)
-Mrufina <- data.frame(lapply(Datos, function(x) {
-  str_replace_all(x, "Â", "")
+Datos <- data.frame(lapply(Datos, function(x) {
+  str_replace_all(x, "Ã‚", "")
 }))
-head(Mrufina)
+head(Datos)
 
-library(camtrapR)
-png(file = "C:/Users/Dilan Vergara/OneDrive/Tesis Maestría Final/Datos/Análisis/Mazama/M_Americana/Mamericana.png", width = 1200, height = 700, res = 150)
-par(mar = c(5, 4, 3, 3) + 0.1)
+
 
 
 activityDensity (recordTable = Datos,
@@ -21,7 +19,7 @@ activityDensity (recordTable = Datos,
                  recordDateTimeFormat = "%d-%m-%Y %H:%M:%S", 
                  plotR = TRUE, 
                  writePNG = TRUE, 
-                 plotDirectory = "C:/users/Dilan Vergara/OneDrive/Tesis Maestría Final/Datos/SP_Todas/ActivityDensity", 
+                 plotDirectory = "C:/users/Dilan Vergara/OneDrive/Tesis MaestrÃ­a Final/Datos/SP_Todas/ActivityDensity", 
                  createDir = TRUE, 
                  pngMaxPix = 1000,
 )
@@ -38,7 +36,7 @@ activityHistogram (recordTable = Datos,
                    recordDateTimeFormat = "%d-%m-%Y %H:%M:%S", 
                    plotR = TRUE, 
                    writePNG = TRUE, 
-                   plotDirectory = "C:/users/Dilan Vergara/OneDrive/Tesis Maestría Final/Datos/SP_Todas/ActivityDensity", 
+                   plotDirectory = "C:/users/Dilan Vergara/OneDrive/Tesis MaestrÃ­a Final/Datos/SP_Todas/ActivityDensity", 
                    createDir = FALSE, 
                    pngMaxPix = 1000,
 )
